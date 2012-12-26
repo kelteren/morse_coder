ce9cc6c2f987163319aa9a5ae40fe17642af4e5b
Stian Hole <stian.hole@gmail.com>
2012-12-26 11:46:27 +0100

    Happy holidays!

--- morse_coder.ino
+++ morse_coder.ino
@@ -1,40 +1,58 @@
 int led = 13;
 
-int dot = 250;
-int dash = 750;
-int space = 750;
-int wordspace = 1750;
+int interval = 250;
 
 void setup() {
+  // init serial for debugging
+  Serial.begin(9600);
+  
+  // init output pin
   pinMode(led, OUTPUT);
 }
 
-void mDot() {
+void writeSignal(int duration) {
   digitalWrite(led, HIGH);
-  delay(dot);
+  delay(duration);
   digitalWrite(led, LOW);
+  delay(interval);
+  //Serial.print("wrote for ");
+  //Serial.println(duration);
 }
 
-void mDash() {
-  digitalWrite(led, HIGH);
-  delay(dash);
-  digitalWrite(led, LOW);
-}
-
-void morse(String text) {
+void morse(char mCode[]) {
   int i = 0;
+  int length = strLength(mCode);
   
-  // my cousin called so this won't work just yet :p
-  while (i < text.length()) {
-    char thisChar = text[i];
-    if (thisChar = ".") {
-      mDot();
-      delay(dot);
+  while(i < length) {
+    
+    if (mCode[i] == '.') {
+      writeSignal(interval);
+    } else if (mCode[i] == '-') {
+      writeSignal(interval * 3);
+    } else if (mCode[i] == ' ') {
+      // space between letters is 3 units, space between words is 7,
+      // but we only do 2 for space and 6 for wordspace as one unit 
+      // delay is already done in the writeSignal() function
+      
+      delay(interval * 2);
+      // Serial.println("space");
+    } else if (mCode[i] == '/') {
+      delay(interval * 6);
+      // Serial.println("wordspace");
     }
+    
+    i++;
   }
 }
 
 void loop () {
-  morse(".");
+  morse("--. --- -../.--- ..- .-../");
 }
 
+// grand theft function - http://arduino.cc/forum/index.php/topic,45301.0.html
+int strLength(char *buffer) {
+  int i=0;
+  while(buffer[i])
+    i++;
+  return i;
+}
